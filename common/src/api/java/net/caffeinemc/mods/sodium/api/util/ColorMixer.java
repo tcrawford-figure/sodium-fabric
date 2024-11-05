975e2785a39948ae5173affca7067c2c332187ed
JellySquid <jellysquid@pm.me>
2024-11-04 19:01:18 -0600

    Fixup documentation in ColorMixer

--- common/src/api/java/net/caffeinemc/mods/sodium/api/util/ColorMixer.java
+++ common/src/api/java/net/caffeinemc/mods/sodium/api/util/ColorMixer.java
@@ -8,33 +8,33 @@ package net.caffeinemc.mods.sodium.api.util;
 public class ColorMixer {
     /**
      * <p>Linearly interpolate between the {@param start} and {@param end} points, represented as packed unsigned 8-bit
-     * values within a 32-bit integer. The result is computed as <pre>(x * a) * (y * (255 - a))</pre>.</p>
+     * values within a 32-bit integer. The result is computed as <pre>(start * weight) + (end * (255 - weight))</pre>.</p>
      *
-     * <p>The results are undefined if {@param a} is not within the interval [0, 255].</p>
+     * <p>The results are undefined if {@param weight} is not within the interval [0, 255].</p>
 
-     * @param color0 The start of the range to interpolate
-     * @param color1 The end of the range to interpolate
+     * @param start The start of the range to interpolate
+     * @param end The end of the range to interpolate
      * @param weight The weight value used to interpolate between color values (in 0..255 range)
      * @return The color that was interpolated between the start and end points
      */
-    public static int mix(int color0, int color1, int weight) {
+    public static int mix(int start, int end, int weight) {
         // Overflow is not possible, so adding the values is fine.
-        return mul(color0, weight) + mul(color1, ColorU8.COMPONENT_MASK - weight);
+        return mul(start, weight) + mul(end, ColorU8.COMPONENT_MASK - weight);
     }
 
     /**
-     * <p>This function is identical to {@link ColorMixer#mix(int, int, int)}, but {@param a} is a normalized
+     * <p>This function is identical to {@link ColorMixer#mix(int, int, int)}, but {@param weight} is a normalized
      * floating-point value within the interval of [0.0, 1.0].</p>
      *
-     * <p>The results are undefined if {@param a} is not within the interval [0.0, 1.0].</p>
+     * <p>The results are undefined if {@param weight} is not within the interval [0.0, 1.0].</p>
      *
-     * @param color0 The start of the range to interpolate
-     * @param color1 The end of the range to interpolate
+     * @param start The start of the range to interpolate
+     * @param end The end of the range to interpolate
      * @param weight The weight value used to interpolate between color values (in 0.0..1.0 range)
      * @return The color that was interpolated between the start and end points
      */
-    public static int mix(int color0, int color1, float weight) {
-        return mix(color0, color1, ColorU8.normalizedFloatToByte(weight));
+    public static int mix(int start, int end, float weight) {
+        return mix(start, end, ColorU8.normalizedFloatToByte(weight));
     }
 
     /**
